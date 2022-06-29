88d660d47ec234a10e0a375d68b10a8a09d09dcd
izadora oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2022-06-29 20:57:37 -0300

    Relatório de erros

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
-<?php
-
-declare(strict_types=1);
-
 use Laminas\Mvc\Application;
 use Laminas\Stdlib\ArrayUtils;
+
+/**
+ * Display all errors when APPLICATION_ENV is development.
+ */
+if ($_SERVER['APPLICATION_ENV'] === 'development') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", '1');
+}
 
 /**
  * This makes our life easier when dealing with paths. Everything is relative
@@ -14,7 +18,7 @@ chdir(dirname(__DIR__));
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (is_string($path) && __FILE__ !== $path && is_file($path)) {
+    if (__FILE__ !== $path && is_file($path)) {
         return false;
     }
     unset($path);
